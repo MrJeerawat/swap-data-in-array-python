bc49206bee866c224f78d7fa02b4f9a3b9bf85e9
MrJeerawat <jeerawat.taen@gmail.com>
2021-12-05 08:10:58 +0700

    clean code

--- main.py
+++ main.py
@@ -1,31 +1,16 @@
-
 import math
-arr = ['a','b','c','d','e','f','g']
+arr = ['a','b','c','d','e','f']
 def swapInArr():
     print(arr)
-    if(len(arr)%2 == 0):
-        halfArr = len(arr)/2
-        halfArr = math.trunc(halfArr)
-        counter = 0
-        while (halfArr>0):
-            first = arr.pop(counter)
-            last = arr.pop((len(arr)-1)-counter)  
-            arr.insert(counter,last ) 
-            arr.insert((len(arr))-counter, first) 
-            halfArr-=1
-            counter+=1
-    else:
-        halfArr = (len(arr)-1)/2
-        halfArr = math.trunc(halfArr)
-        counter = 0
-        while (halfArr>0):
-            first = arr.pop(counter)
-            last = arr.pop((len(arr)-1)-counter)  
-            arr.insert(counter,last ) 
-            arr.insert((len(arr))-counter, first) 
-            halfArr -= 1
-            counter += 1
-            
+    halfArr = len(arr)/2
+    halfArr = math.trunc(halfArr)
+    counter = 0
+    while (halfArr>0):
+        first = arr.pop(counter)
+        last = arr.pop((len(arr)-1)-counter)  
+        arr.insert(counter,last ) 
+        arr.insert((len(arr))-counter, first) 
+        halfArr-=1
+        counter+=1
     print((arr))
-    
 swapInArr()
